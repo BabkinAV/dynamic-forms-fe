4b2177e0c146165c6a1924489db0dd41341e35e7
Andrey Babkin <babkin.andrey@gmail.com>
2023-04-15 14:28:06 +0300

    changed env name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home({customerData}: {customerData: Customer[]}) {
 export async function getServerSideProps() {
 
   let res = await axios.get<Customer[]>(
-    `${process.env.BACKEND_ADDRESS ?? 'http://backend:8080'}/customers?_sort=created_at&_order=desc`
+    `${process.env.NEXT_PUBLIC_BACKEND_ADDRESS ?? 'http://backend:8080'}/customers?_sort=created_at&_order=desc`
   );
 	const customerData = res.data;
 
